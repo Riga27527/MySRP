015a1bbdf929207a99aeed70fe548ceae5ac8cad
Riga27527 <596847942@qq.com>
2023-02-19 20:45:04 +0800

    add alpha blend and alpha test

--- Assets/SRP01/Shaders/UnlitPass.hlsl
+++ Assets/SRP01/Shaders/UnlitPass.hlsl
@@ -7,15 +7,23 @@
 //     float4 _BaseColor;
 // CBUFFER_END
 
+TEXTURE2D(_BaseMap);
+SAMPLER(sampler_BaseMap);
+
 // 4. setup UnityPerMaterial buffer property
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+
 UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
+UNITY_DEFINE_INSTANCED_PROP(float, _Cutoff)
+
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 // 3. add UNITY_VERTEX_INPUT_INSTANCE_ID
 struct Attributes
 {
     float3 positionOS : POSITION;
+    float2 baseUV : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -35,6 +43,9 @@ Varyings UnlitPassVertex(Attributes input)
     UNITY_TRANSFER_INSTANCE_ID(input, output);
     float3 positionWS = TransformObjectToWorld(input.positionOS);
     output.positionCS = TransformWorldToHClip(positionWS);
+
+    float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    output.baseUV = input.baseUV * baseST.xy + baseST.zw;
     return output;
 }
 
@@ -42,7 +53,16 @@ Varyings UnlitPassVertex(Attributes input)
 float4 UnlitPassFragment(Varyings input) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(input);
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
+    float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 color = baseMap * baseColor;
+    
+#if defined(_CLIPPING)
+    float cutoff_threshold = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff);
+    clip(color.a - cutoff_threshold);
+#endif
+
+    return color;
 }
 
 
